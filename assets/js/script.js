c22e43a1ee704737752a5fd6f9137b35f13959c4
Istem Karakas <istem.karakas@gmail.com>
2024-01-27 22:54:44 +0000

    Change style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const tipsContainer = document.getElementById("tips-container")
 const closeContainer = document.getElementById("close-container")
-const tipBtn = document.getElementById("tips-btn")
+const tipsBtn = document.getElementById("tips-btn")
 tipsBtn.addEventListener("click", function(){
     tipsContainer.style.display = "flex"
     closeContainer.addEventListener("click", function(){
