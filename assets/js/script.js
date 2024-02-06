e94c800e9a6b1ebcc11019d4983f5daf60742341
Istem Karakas <istem.karakas@gmail.com>
2024-02-06 11:00:12 +0000

    Add hide class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,17 @@
 const tipsContainer = document.getElementById("tips-container")
 const closeContainer = document.getElementById("close-container")
+const cardContainer = document.getElementById("card-container")
+const controlsContainer = document.getElementById("controls-container")
 const tipsBtn = document.getElementById("tips-btn")
 tipsBtn.addEventListener("click", function(){
-    tipsContainer.style.display = "flex"
+    tipsContainer.classList.remove("hide")
     closeContainer.addEventListener("click", function(){
-        tipsContainer.style.display = "none"
+        tipsContainer.classList.add("hide")
     })
+})
+const startBtn = document.getElementById("start") 
+startBtn.addEventListener("click", function(){
+    cardContainer.classList.remove("hide")
+    controlsContainer.classList.add("hide")
 })
 
