8a6dd770242e1d7b420c9679ddfc2ddf517f60cf
Istem Karakas <istem.karakas@gmail.com>
2024-02-12 00:40:51 +0000

    It was checked whether the data-matches of the cards opened were equal to each other.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,18 +6,18 @@ const controlsContainer = document.getElementById("controls-container")
 
 
 let cardFrontTemplate = [
-    `<div class="card view" data-match="1"> 3 <sup> 2 </sup></div>`,
-    `<div class="card view" data-match="1"> 9 </div>`,
-    `<div class="card view" data-match="2"> 4 <sup> 1 </sup></div>`,
-    `<div class="card view" data-match="2"> 4 </div>`,
-    `<div class="card view" data-match="3"> 3 <sup> 3 </sup></div>`,
-    `<div class="card view" data-match="3"> 27 </div>`,
-    `<div class="card view" data-match="4"> 1 <sup> 3 </sup></div>`,
-    `<div class="card view" data-match="4"> 1 </div>`,
-    `<div class="card view" data-match="5"> 7 <sup> 2 </sup></div>`,
-    `<div class="card view" data-match="5"> 49 </div>`,
-    `<div class="card view" data-match="6"> 6 <sup> 2 </sup></div>`,
-    `<div class="card view" data-match="6"> 36 </div>`
+    `<span data-match="1"> 3 <sup> 2 </sup></span>`,
+    `<span data-match="1"> 9 </span>`,
+    `<span data-match="2"> 4 <sup> 1 </sup></span>`,
+    `<span data-match="2"> 4 </span>`,
+    `<span data-match="3"> 3 <sup> 3 </sup></span>`,
+    `<span data-match="3"> 27 </span>`,
+    `<span data-match="4"> 1 <sup> 3 </sup></span>`,
+    `<span data-match="4"> 1 </span>`,
+    `<span data-match="5"> 7 <sup> 2 </sup></span>`,
+    `<span data-match="5"> 49 </span>`,
+    `<span data-match="6"> 6 <sup> 2 </sup></span>`,
+    `<span data-match="6"> 36 </span>`
 ]
 
 let mediumCardFrontTemplate = [
@@ -44,7 +44,7 @@ memorySettingsGrid.addEventListener("change", function () {
     console.log("templateToUse:", templateToUse)
     if (templateToUse === "Medium") {
         cardFrontTemplate = mediumCardFrontTemplate
-    }  
+    }
 });
 
 const tipsBtn = document.getElementById("tips-btn")
@@ -87,21 +87,21 @@ function shuffleCards() {
 
 //Card front view
 const cardArray = document.querySelectorAll(".card");
-for (let i = 0; i < cardArray.length; i++) {
-    
-    cardArray[i].addEventListener("click", function (event) {
+    for (let i = 0; i < cardArray.length; i++) {
+
+    cardArray[i].addEventListener("click", function(){
         const newCard = document.createElement("div");
         newCard.innerHTML = cardFrontTemplate[i % cardFrontTemplate.length]; // Use modulus operator to cycle through cardTemplate array
-        newCard.classList.add("front","view", "card");
+        newCard.classList.add("front", "view", "card");
         const selectedCard = event.currentTarget;
         selectedCard.append(newCard);
         //console.log(selectedCard);
 
         cardEvents(newCard, selectedCard)
-    });
-
-    return
+    }); 
+    
 }
+
 
 
 // Restart Button
@@ -115,23 +115,61 @@ restartBtn.addEventListener("click", function () {
 
 //Card Events
 function cardEvents(newCard, selectedCard) {
-    
+
     //Is there an open card?
     const openedCard = document.querySelectorAll(".open")
     console.log(openedCard);
 
-    if(openedCard.length === 0) { //It means there are no open cards.
+    //If the opened card is clicked again, no action will be taken.
+    if (selectedCard.classList.contains('open') === true){
+        selectedCard.removeEventListener("click", cardEvents) 
+    }
+    
+    if (openedCard.length === 0) { //It means there are no open cards.
         selectedCard.classList.add("open")
+    } else {
+        selectedCard.classList.add("open")
+    }
+    
+    let timeoutID = setTimeout(() => {
+        setTimeout(() => {
+            selectedCard.classList.remove("open")
+            newCard.remove()
+        })
+    }, 1500); 
+    
+
+
+    
+    //We had a card that was opened before and we opened the one that was opened later.
+
+    //Are the data-matches of the opened cards equal?
+    const firstCard = document.querySelectorAll("span")[1]
+    const secondCard = document.querySelectorAll("span")[2]
+    
+    if (openedCard.length !== 0 && firstCard.dataset.match === secondCard.dataset.match) {
+
+        console.log(firstCard.dataset.match, secondCard.dataset.match )
+        firstCard.classList.add("matched")
+        secondCard.classList.add("matched")
+
+        firstCard.classList.remove("open")
+        secondCard.classList.remove("open")
+
+        clearTimeout(timeoutID)
+        
 
         setTimeout(() => {
-        selectedCard.classList.remove("open")
-        newCard.remove()
+            firstCard.removeEventListener("click", cardEvents)
+            secondCard.removeEventListener("click", cardEvents)
+        },1000)
 
-        }, 1500)
     }
-    //We had a card that was opened before and we opened the one that was opened later.
-    selectedCard.classList.add("open")
+
+
 }
+
+
 
 
 
